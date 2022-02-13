c37330288b1c6f870232714fea12ac4dfbb8422f
neeti.pp@pathak <neeti.pp@gmail.com>
2022-02-13 13:42:17 +0530

    status

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from  '@react-navigation/bottom-tabs';
+import TransScr from './screens/transaction';
+import SearchScr from './screens/search';
+export default class App extends React.Component{
+  
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text>Wily App</Text>
+        <NavigationContainer>
+           <Tab.Navigator>
+             <Tab.Screen name='trans' component='TransScr'/>
+             <Tab.Screen name='search' component='SearchScr'/>
+           </Tab.Navigator>
+        </NavigationContainer>
+       
+      </View>
+       );
+  }
+  
+ 
 }
+
+
+const Tab=createBottomTabNavigator();
+
+
+
+
 
 const styles = StyleSheet.create({
   container: {
